bdfc727c4ad0c1b97815ae682107e9a4d21919e3
Vlad Hristov <vpchar@gmail.com>
2022-12-19 08:10:11 +0200

    Add Settings dialog - CSS

--- rockPapperScissors.js
+++ rockPapperScissors.js
@@ -1,8 +1,8 @@
 let rules = {
      logic: {
-        f1beats: 2, //by default rock beats scissors etc.
+        f1beats: 2, 
         f2beats: 3,
-        f3beats: 1
+        f3beats: 1,
     },
 
      names:{
@@ -33,9 +33,66 @@ let rules = {
         localStorage.removeItem("names");
     } 
 }
-console.log('test')
-rules.save();
-rules.load();
-console.log(rules.names)
-console.log(rules.logic)
-rules.defaults();
+let game = {
+    userChoice: 0,
+    computerChoice: 0,
+    userSelected(e){
+        if(e === 'a') {this.userChoice = 1}
+        else if(e === 'b'){ this.userChoice = 2}
+        else if( e === 'c'){ this.userChoice = 3}
+        else {userChoice = 0;}
+        // console.log(e);
+        // console.log(this.userChoice)
+    },
+    computerSelected(){
+        this.computerChoice= (Math.floor(Math.random() * 3))+1;
+        //console.log(this.computerChoice)
+    }
+}
+
+//attach click events
+const buttons = document.querySelectorAll('.button');
+buttons.forEach(button =>{
+    // console.log(button.id)
+    button.addEventListener('click',(e)=>{
+        //button.id clicked
+        game.userSelected(button.id);
+        game.computerSelected();
+    })
+});
+
+//settings button
+const settings = document.getElementById('settings');
+const modal = document.querySelector('.modal');
+const board = document.querySelector('.board');
+const defaultBtn = document.getElementById('defaults');
+
+const f1 = document.getElementById('f1');
+const f2 = document.getElementById('f2');
+const f3 = document.getElementById('f3');
+const b1 = document.getElementById('bu1');
+const b2 = document.getElementById('bu2');
+const b3 = document.getElementById('bu3');
+
+function closeModal(){
+    modal.style.display="none";
+    board.style.display="flex";
+}
+defaultBtn.addEventListener('click',(e)=>{
+    //restore defaults
+
+});
+
+settings.addEventListener('click', (e) =>{
+    //show modal   
+    board.style.display="none"; 
+    modal.style.display="flex";
+    //load current rules
+    f1.value=rules.names.f1name;
+    f2.value=rules.names.f2name;
+    f3.value=rules.names.f3name;
+
+    b1.value=rules.logic.f1beats;
+    b2.value=rules.logic.f2beats;
+    b3.value=rules.logic.f3beats;
+});
